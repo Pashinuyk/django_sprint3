f512fcb898c301d9fc35080a714964ebc660aeb9
Pashinuyk <pavelkiselev2004@gmail.com>
2026-05-23 20:37:51 +1000

    Second

--- blogicum/blog/admin.py
+++ blogicum/blog/admin.py
@@ -2,9 +2,33 @@
 from django.contrib import admin
 
 # Из модуля models импортируем модель Category...
-from .models import Post, Category, Location
+from .models import User, Post, Category, Location
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'text',
+        'pub_date',
+        'author',
+        'location',
+        'is_published',
+        'category'
+    )
+    list_editable = (
+        'title',
+        'pub_date',
+        'is_published'
+    )
+    search_fields = ('title',)
+    list_filter = ('category',)
+    list_display_links = ('text', 'author', 'location', 'category')
+    #filter_horizontal = ('toppings',)
+
 
 # ...и регистрируем её в админке:
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
 admin.site.register(Category)
 admin.site.register(Location)
+
+admin.site.empty_value_display = 'Не задано'
--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -2,65 +2,88 @@ from django.db import models
 from core.models import PublishedModel
 from django.contrib.auth import get_user_model
 
-
 User = get_user_model()
+
+"""
+class PostAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'text',
+        'pub_date',
+        'author',
+        'location',
+        'category'
+    )
+    list_editable = (
+        'title',
+        'text',
+        'pub_date'
+    )
+    search_fields = ('title',)
+    list_filter = ('category',)
+    list_display_links = ('author', 'location', 'category')
+    #filter_horizontal = ('toppings',)
+
+"""    
 
 
 class Post(PublishedModel):
-    title = models.CharField(max_length=256, verbose_name='Заголовок')
-    text = models.TextField(verbose_name='Текст')
-    pub_date = models.DateTimeField(verbose_name='Дата и время публикации', 
-                                    help_text='Если установить дату и время ' \
-                                    'в будущем — можно делать отложенные публикации.')
+    title = models.CharField(max_length=256, verbose_name="Заголовок")
+    text = models.TextField(verbose_name="Текст")
+    pub_date = models.DateTimeField(
+        verbose_name="Дата и время публикации",
+        help_text="Если установить дату и время "
+        "в будущем — можно делать отложенные "
+        "публикации.",
+    )
     author = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        null=False,
-        verbose_name='Автор публикации'
+        User, on_delete=models.CASCADE, null=False,
+        verbose_name="Автор публикации"
     )
     location = models.ForeignKey(
-        'Location',
+        "Location",
         on_delete=models.SET_NULL,
         null=True,
         blank=True,
-        verbose_name='Местоположение'
+        verbose_name="Местоположение",
     )
     category = models.ForeignKey(
-        'Category',
-        on_delete=models.SET_NULL,
-        null=True,
-        verbose_name='Категория'
+        "Category", on_delete=models.SET_NULL, null=True,
+        verbose_name="Категория"
     )
 
     class Meta:
-        verbose_name = 'публикация'
-        verbose_name_plural = 'Публикации'
+        verbose_name = "публикация"
+        verbose_name_plural = "Публикации"
 
     def __str__(self):
         return self.title
 
 
 class Category(PublishedModel):
-    title = models.CharField(max_length=256, verbose_name='Заголовок')
-    description = models.TextField(verbose_name='Описание')
-    slug = models.SlugField(unique=True, verbose_name='Идентификатор', 
-                            help_text='Идентификатор страницы для URL; ' \
-                            'разрешены символы латиницы, цифры, дефис и подчёркивание.')
+    title = models.CharField(max_length=256, verbose_name="Заголовок")
+    description = models.TextField(verbose_name="Описание")
+    slug = models.SlugField(
+        unique=True,
+        verbose_name="Идентификатор",
+        help_text="Идентификатор страницы для URL; "
+        "разрешены символы латиницы, цифры, дефис и подчёркивание.",
+    )
 
     class Meta:
-        verbose_name = 'категория'
-        verbose_name_plural = 'Категории'
+        verbose_name = "категория"
+        verbose_name_plural = "Категории"
 
     def __str__(self):
         return self.title
 
 
 class Location(PublishedModel):
-    name = models.CharField(max_length=256, verbose_name='Название места')
+    name = models.CharField(max_length=256, verbose_name="Название места")
 
     class Meta:
-        verbose_name = 'местоположение'
-        verbose_name_plural = 'Местоположения'
+        verbose_name = "местоположение"
+        verbose_name_plural = "Местоположения"
 
     def __str__(self):
         return self.name
